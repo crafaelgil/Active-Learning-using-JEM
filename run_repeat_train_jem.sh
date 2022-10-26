13d60f363eb9f1866dd0c85e19d13efc2ab842f5
crafaelgil <crafaelgil@gmail.com>
2022-10-26 12:04:15 +0900

    Add experiment ideas

--- run_repeat_train_jem.sh
+++ run_repeat_train_jem.sh
@@ -3,26 +3,26 @@ pip install -r requirements.txt ;
 for i in `seq 1`
 do
     # Train
-    # python train_jem.py \
-    # --basenet wideresnet \
-    # --lr .0001 \
-    # --dataset bloodmnist \
-    # --n_classes 8 \
-    # --optimizer adam \
-    # --p_x_weight 1.0 \
-    # --p_y_given_x_weight 1.0 \
-    # --p_x_y_weight 0.0 \
-    # --sigma .03 \
-    # --save_dir jem_bloodmnist/seed$i/ \
-    # --warmup_iters 1000 \
-    # --buffer_size 10000 \
-    # --n_epochs 100 \
-    # --n_ch 3 \
-    # --batch_size 128 \
-    # --im_sz 28 \
-    # --labels_per_class 1 \
-    # --semisupervision_seed $i \
-    # --print_to_log ;
+    python train_jem.py \
+    --basenet wideresnet \
+    --lr .0001 \
+    --dataset bloodmnist \
+    --n_classes 8 \
+    --optimizer adam \
+    --p_x_weight 1.0 \
+    --p_y_given_x_weight 1.0 \
+    --p_x_y_weight 0.0 \
+    --sigma .03 \
+    --save_dir jem_bloodmnist/seed$i/ \
+    --warmup_iters 1000 \
+    --buffer_size 10000 \
+    --n_epochs 100 \
+    --n_ch 3 \
+    --batch_size 128 \
+    --im_sz 28 \
+    --labels_per_class 1 \
+    --semisupervision_seed $i \
+    --print_to_log ;
     # Test
     python test_jem.py \
     --basenet wideresnet \
